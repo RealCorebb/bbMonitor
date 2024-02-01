559343ac9db2389646adc6c3ec953c0d6c09948e
RealCorebb <coreoobb@qq.com>
2024-02-01 14:46:41 +0800

    WebSockets Protocol

--- bbMonitor.ino
+++ bbMonitor.ino
@@ -1,14 +1,81 @@
 #include <Preferences.h>
 #include <ESPmDNS.h>
 #include <WiFi.h>
+#include <WebSocketsServer.h>
+#include <ArduinoJson.h>
 
 #define P1 38
 #define P2 5
+#define P3 6
+#define P4 7
+#define P5 15
+#define P6 16
+#define P7 17
+#define P8 18
+#define P9 8
+#define P10 9
+#define P11 10
+#define P12 11
+#define P13 12
+#define P14 13
+#define P15 14
+#define P16 21
+#define P17 35
+#define P18 36
+const int pins[] = {P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18};
+
 #define MAX 250
 
 Preferences preferences;
 bool mdnsOn = false;
 
+WebSocketsServer webSocket = WebSocketsServer(80);
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_TEXT:
+      handleWebSocketText(payload, length);
+      break;
+  }
+}
+
+void handleWebSocketText(uint8_t *payload, size_t length) {
+  // Parse JSON data
+  DynamicJsonDocument jsonDoc(512);
+  DeserializationError error = deserializeJson(jsonDoc, payload, length);
+
+  if (error) {
+    Serial.println("Failed to parse JSON");
+    return;
+  }
+
+  // Check if the "data" key exists
+  if (jsonDoc.containsKey("data")) {
+    JsonObject data = jsonDoc["data"];
+
+    // Iterate through keys in "data" and set analogWrite values for defined pins
+    for (JsonPair kv : data) {
+      String pinName = kv.key().c_str();
+
+      // Find the corresponding pin based on the key
+      int pinIndex = pinName.substring(1).toInt() - 1;
+
+      // Check if the pin index is within the valid range
+      if (pinIndex >= 0 && pinIndex < sizeof(pins) / sizeof(pins[0])) {
+        int pinValue = kv.value();
+        analogWrite(pins[pinIndex], pinValue);
+        Serial.print("Set PWM value for Pin ");
+        Serial.print(pinName);
+        Serial.print(" to ");
+        Serial.println(pinValue);
+      } else {
+        Serial.println("Invalid Pin: " + pinName);
+      }
+    }
+  } else {
+    Serial.println("No 'data' key found in JSON");
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -36,14 +103,6 @@ void setupMDNS(){
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(mdnsOn);
   if(!mdnsOn) setupMDNS();
-  analogWrite(P1,MAX * 1);
-  delay(1000);
-  analogWrite(P1,MAX * 0.5);
-  delay(1000);
-  analogWrite(P1,MAX * 0);
-  delay(1000);
-  analogWrite(P1,MAX * 0.5);
-  delay(1000);
+  webSocket.loop();
 }
