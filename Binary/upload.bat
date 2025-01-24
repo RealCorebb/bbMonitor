0dd7b7945d7f808087aa2997a5806a2289f9bd2c
RealCorebb <33654314+RealCorebb@users.noreply.github.com>
2025-01-24 16:33:22 +0800

    binary firmware

---
+++ Binary/upload.bat
@@ -0,0 +1,1 @@
+"C:\Users\coreb\AppData\Local\Arduino15\packages\esp32\tools\esptool_py\4.5.1/esptool.exe" --chip esp32s3 --port "COM14" --baud 921600  --before default_reset --after hard_reset write_flash  -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x0 "bbMonitor.ino.bootloader.bin" 0x8000 "bbMonitor.ino.partitions.bin" 0xe000 "boot_app0.bin" 0x10000 "bbMonitor.ino.bin"
